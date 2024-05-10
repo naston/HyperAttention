c22f154e23b9a1fe03c1cb9370bd2095c84e24db
naston <nathan@planetpaull.com>
2024-05-09 23:43:48 -0500

    initialize codebase

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# HyperAttention
-Hyperbolic Attention
+# Hyperbolic Attention
+
+This project aims to investigate the use of `tanh` as the basis for formulating `softmax` instead of the `logistic function (sigmoid)`.
+
+
+## TODO:
+- Finalize model code
+    - make sure implementation can be trained
+- Create training code
+    - may just be able to port over some code from my previous work
+- Finalize dataset selection
+    - C4 or TinyStories
+- Finalize model hyper-params
+- Run tests
---
+++ src/hyperatt/model.py
@@ -0,0 +1,38 @@
+import math
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+import fairscale.nn.model_parallel.initialize as fs_init
+import torch
+import torch.nn.functional as F
+from fairscale.nn.model_parallel.layers import (
+    ColumnParallelLinear,
+    ParallelEmbedding,
+    RowParallelLinear,
+)
+from torch import nn
+
+from .model_factory import TransformerBlock, RMSNorm, precompute_freqs_cis
+
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32
+    n_kv_heads: Optional[int] = None
+    vocab_size: int = -1  # defined later by tokenizer
+    multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+
+class LlamaModel(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
